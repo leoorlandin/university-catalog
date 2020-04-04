68c88440126b49ef7b36f72dc88820b7d03df62e
unknown <leonardo.1117@hotmail.com>
2020-04-04 13:55:58 -0300

    criado rota para formulario de cadastro de professor

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const teachers = require('./teachers')
 
 
 routes.get('/', function(req, res){
@@ -9,6 +10,12 @@ routes.get('/', function(req, res){
 routes.get('/teachers', function(req, res){
   return res.render("teachers/index")
 })
+
+routes.get('/teachers/create', function(req, res){
+  return res.render("teachers/create")
+})
+
+routes.post('/teachers', teachers.post)
 
 routes.get('/members', function(req, res){
   return res.send("members")
