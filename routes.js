1e42408e3e54f552c49ceb97f6a517e24e299b47
unknown <leonardo.1117@hotmail.com>
2020-04-05 06:56:18 -0300

    criado metodo post para formulario de cadastro de professores/adicinado arquivo de rotas

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = express.Router()
-// const teachers = require('./teachers')
+const teachers = require('./teachers')
 
 
 routes.get('/', function(req, res){
@@ -15,14 +15,8 @@ routes.get('/teachers/create', function(req, res){
   return res.render("teachers/create")
 })
 
-routes.post('/teachers', function(req, res){
-  
-  if(req.body.name != ""){
-    return res.send(req.body.name)
-  }
+routes.post('/teachers', teachers.post)
 
-
-})
 
 routes.get('/members', function(req, res){
   return res.send("members")
