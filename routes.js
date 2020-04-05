516b58c394ed2ded36af32d4a0098c01d8054b34
unknown <leonardo.1117@hotmail.com>
2020-04-05 05:44:22 -0300

    ajuste nas opções de grau de escolaridade

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = express.Router()
-const teachers = require('./teachers')
+// const teachers = require('./teachers')
 
 
 routes.get('/', function(req, res){
@@ -15,7 +15,14 @@ routes.get('/teachers/create', function(req, res){
   return res.render("teachers/create")
 })
 
-routes.post('/teachers', teachers.post)
+routes.post('/teachers', function(req, res){
+  
+  if(req.body.name != ""){
+    return res.send(req.body.name)
+  }
+
+
+})
 
 routes.get('/members', function(req, res){
   return res.send("members")
--- teachers.js
+++ teachers.js
@@ -1,39 +1,39 @@
-const fs = require('fs')
-const data = require('./data.json')
+// const fs = require('fs')
+// const data = require('./data.json')
 
-exports.post = function (req, res) {
+// exports.post = function (req, res) {
 
-  const keys = Object.keys(req.body)
+//   const keys = Object.keys(req.body)
 
-  for (key of keys) {
-    if (req.body[key] == "") {
-      return res.send('Por favor, preencha todos os campos!')
-    }
-  }
+//   for (key of keys) {
+//     if (req.body[key] == "") {
+//       return res.send('Por favor, preencha todos os campos!')
+//     }
+//   }
 
-  let { avatar_url, name, birth, formation, lecture, area } = req.body
+//   let { avatar_url, name, birth, formation, lecture, area } = req.body
 
-  birth = Date.parse(birth)
-  const created_at = Date.now()
-  const id = Number(data.teachers.length + 1)
+//   birth = Date.parse(birth)
+//   const created_at = Date.now()
+//   const id = Number(data.teachers.length + 1)
 
-  data.teachers.push({
-    id,
-    avatar_url,
-    name,
-    birth,
-    formation,
-    lecture,
-    area,
-    created_at
-  })
+//   data.teachers.push({
+//     id,
+//     avatar_url,
+//     name,
+//     birth,
+//     formation,
+//     lecture,
+//     area,
+//     created_at
+//   })
 
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
-    if(err) return res.send("Write file error")
+//   fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+//     if(err) return res.send("Write file error")
 
-    return res.redirect("/teachers")
-  })
+//     return res.redirect("/teachers")
+//   })
 
-  return res.send(req.body)
+//   // return res.send(req.body)
 
-}
+// }
