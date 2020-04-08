ec0ef2e75c99feef78f04221880965598cd760d5
unknown <leonardo.1117@hotmail.com>
2020-04-08 13:25:49 -0300

    página index reformulada para exibição de professores cadastrados

--- teachers.js
+++ teachers.js
@@ -2,6 +2,17 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./util')
 
+//INDEX
+exports.index = function (req, res) {
+
+
+  return res.render("teachers/index", { teachers: data.teachers })
+
+
+}
+
+
+
 //SHOW
 exports.show = function (req, res) {
 
@@ -100,7 +111,8 @@ exports.put = function (req, res) {
   const teacher = {
     ...foundTeacher,
     ...req.body,
-    birth: Date.parse(req.body.birth)
+    birth: Date.parse(req.body.birth),
+    id: Number(req.body.id)
   }
 
   data.teachers[index] = teacher
